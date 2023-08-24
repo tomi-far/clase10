7d4a56775fb33d084cf60b946403578bd2bdd812
tomi-far <140024630+tomi-far@users.noreply.github.com>
2023-08-24 02:28:44 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+//Enunciado de variables
+var nombre = "Tomás"
+var apellido = "Farías" 
+
+//Mostrar variables
+console.log(nombre + apellido)
+
+//Mostrar variables espaciadas
+console.log(nombre + " " + apellido)
+
+//Variables numéricas
+var nacimientomessi = 1987
+var ganamosmundial = 2022
+var suma = nacimientomessi+ganamosmundial
+var unnumero = 1000
+
+//Mostrar suma
+console.log(suma)
+
+//Sumar suma y un número y mostrar
+console.log(suma + unnumero)
